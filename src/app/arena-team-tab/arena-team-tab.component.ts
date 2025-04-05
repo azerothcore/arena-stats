4d520b016a68f453ecebdd4e445bb441d005e965
Helias <stefanoborzi32@gmail.com>
2025-04-05 19:48:41 +0200

    refactor: migrate inputs to singlas

--- src/app/arena-team-tab/arena-team-tab.component.ts
+++ src/app/arena-team-tab/arena-team-tab.component.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ArenaTeamService } from '../arena-team/arena-team.service';
 import { PlayerIconComponent } from '../player-icons/player-icons.component';
 import { ArenaTeam } from '../types/arena-team.type';
 
 @Component({
-    selector: 'app-arena-team-tab',
-    imports: [TabsModule, CommonModule, PlayerIconComponent],
-    templateUrl: './arena-team-tab.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-arena-team-tab',
+  imports: [TabsModule, CommonModule, PlayerIconComponent],
+  templateUrl: './arena-team-tab.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArenaTeamTabComponent {
-  @Input() teams: ArenaTeam[];
-  @Input() isSolo = false;
+  readonly teams = input<ArenaTeam[]>([]);
+  readonly isSolo = input(false);
 
   protected readonly service: ArenaTeamService = inject(ArenaTeamService);
 }
